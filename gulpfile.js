7fa46a3f2c336a34b8af85b4f88c3fa7e0dbe185
TheAnnatolich <a_y_nas_na@ukr.net>
2020-05-07 14:16:12 +0300

    feat: add normalize, grid, variables, header...

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const {src, dest, watch} = require("gulp"),
+      browserSync = require("browser-sync").create(),
+      sass = require("gulp-sass");
+
+
+ function bs() {
+   serveSass();
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  watch("./*.html").on("change", browserSync.reload);
+  watch("./sass/**/*.sass" , serveSass);
+  watch("./js/*.js").on("change", browserSync.reload);
+};
+
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
