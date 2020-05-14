4e7fb1f114567d7ff1e7f5b01f911bb3e746bc44
TheAnnatolich <a_y_nas_na@ukr.net>
2020-05-14 20:33:04 +0300

    feat: rewrite js to jquary

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeModal = document.querySelector('.modal__close');
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     const modal = document.querySelector('.modal');
+//     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//     const closeModal = document.querySelector('.modal__close');
+  
+//     modalBtn.forEach(element => {
+//         element.addEventListener('click', switchModal);
+//     }); 
+//     modal.addEventListener('click', (e) => {
+//         if(e.target === modal) {
+//             switchModal();
+//         }
+//     });
+//     closeModal.addEventListener('click', switchModal);
+    
+    // document.addEventListener('keydown', (e) => {
+    //   if (e.code === "Escape") {
+    //     switchModal();
+    //   }
+    // })
+// });
+
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeModal = $('.modal__close');
     const switchModal = () => {
-        modal.classList.toggle('modal--visible');
-    }
-    modalBtn.forEach(element => {
-        element.addEventListener('click', switchModal);
-    }); 
+        modal.toggleClass('modal--visible');
+    };
+    modalBtn.on('click', function(){
+        modal.toggleClass('modal--visible');
+    });
+    closeModal.on('click', function(){
+        modal.toggleClass('modal--visible');
+    });
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape") {
+          switchModal();
+        }
+    })
     modal.addEventListener('click', (e) => {
-        if(e.target === modal) {
-            switchModal();
+     if(e.target === modal) {
+          modal.removeClass('modal--visible');
         }
     });
-    closeModal.addEventListener('click', switchModal);
-    
-    document.addEventListener('keydown', (e) => {
-      if (e.code === "Escape") {
-        switchModal();
-      }
-    })
 });
